5ac9f9efdc7a569cbea626caa0a670fae9e939cb
0cin05 <widawski.nico@gmail.com>
2026-06-11 22:03:29 +0200

    #1: auto detect hardware

---
+++ TemperaturMonitor/Models/HardwareEntry.cs
@@ -0,0 +1,21 @@
+﻿using OpenHardwareMonitor.Hardware;
+
+public class HardwareEntry
+{
+    public string Name { get; set; } = "";
+    public HardwareType Type { get; set; }
+    
+    public bool HasTemp { get; set; }
+    public bool HasLoad { get; set; }
+    public bool HasFan  { get; set; }
+    
+    public float Temp   { get; set; }
+    public float Load   { get; set; }
+    public float FanRpm { get; set; }
+    
+    public double MaxTemp { get; set; }
+    public List<(DateTime t, float v)> Points { get; set; } = new();
+    
+    public IHardware Hardware { get; set; } = null!;
+    public IHardware? FanHardware { get; set; }
+}
